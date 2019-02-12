416646d902810cbdb1d4c3233577e9d1d470aa03
sylatupa <sylatupa@asu.edu>
2019-02-12 00:09:52 -0700

    Updating shell script and default loadbangs for knob values

--- scrptRnsPD.sh
+++ scrptRnsPD.sh
@@ -1,3 +1,3 @@
-
 #sudo pd -nogui -audiooutdev 3,4 -audioindev 3,4 /home/pi/Tools_And_Such/Patches_Working/main_final_version.pd
-sudo pd -nogui -audioindev 3,4 -audiooutdev 3,4  /home/pi/Tools_And_Such/Patches_Working/C_DelayAuto/main_final_version.pd &
+sudo pd -nogui -audioindev 1,2 -audiooutdev 1,2 $HOME/Digital-Culture-Sound-Client/Pure_Data_Patches/E_Basic_Poly/main.pd &
+sudo pd -nogui -audioindev 1,2 -audiooutdev 1,2 $HOME/Digital-Culture-Sound-Client/Pure_Data_Patches/mother-desktop-raspad-kordbot.pd
